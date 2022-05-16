c543c49e338019d3f5959dea54e1bd46a98f55b6
AsenTerziyski <asenterziyski@gmail.com>
2022-05-16 07:40:50 +0300

    test

--- Kotlin/src/com/google/kotlin/SECTION1/s6/29FunctionProgramming.kt
+++ Kotlin/src/com/google/kotlin/SECTION1/s6/29FunctionProgramming.kt
@@ -1,2 +1,21 @@
 package com.google.kotlin.SECTION1.s6
 
+fun main(args: Array<String>) {
+
+    val circleArea: (Double) -> Double = { r: Double -> Math.PI * r }
+
+    val area = findCircleArea(5, circleArea)
+    println(area)
+
+    // void function
+    val result: (Int, Int) -> Unit = { a: Int, b: Int -> println(a + b) }
+    functionAddAndPrint(2,5, result)
+}
+
+fun functionAddAndPrint(a: Int, b: Int, myFunction: (Int, Int) -> Unit) {
+    return myFunction(a,b)
+}
+
+fun findCircleArea(radius: Int, circleArea: (Double) -> Double): Double {
+    return circleArea(1.0)
+}
